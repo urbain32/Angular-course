e8b9c8f9df60877ff5e0a679da0bcf02035caca5
urbain32 <urbainq32@gmail.com>
2023-11-13 10:46:39 +0200

    creating variable countOnce

--- Lecture/Lecture 14(Digest cycle)/app.js
+++ Lecture/Lecture 14(Digest cycle)/app.js
@@ -7,9 +7,12 @@
 
   CounterController.$inject = ["$scope"];
   function CounterController($scope) {
-    $scope.onceCount = 0;
+    $scope.onceCounter = 0;
     $scope.showNumberOfWatchers = function () {
       console.log("Number of watchers:", $scope.$$watchersCount);
+    };
+    $scope.countOnce = function () {
+      $scope.onceCounter = 1;
     };
   }
 })();
