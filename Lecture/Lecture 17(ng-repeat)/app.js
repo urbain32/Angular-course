443ccd57a1c62344c68dd2d836ecc77b9f402d39
urbain32 <urbainq32@gmail.com>
2023-11-14 10:39:08 +0200

    re arrangig the lecture17

--- Lecture/Lecture 17(ng-repeat)/app.js
+++ Lecture/Lecture 17(ng-repeat)/app.js
@@ -39,14 +39,5 @@
   function ShoppingListController($scope) {
     $scope.shoppingList1 = shoppingList1;
     $scope.shoppingList2 = shoppingList2;
-
-    $scope.addToList = function () {
-      var newItem = {
-        name: $scope.newItemName,
-        quantity: $scope.newItemQuantity,
-      };
-
-      $scope.shoppingList2.push(newItem);
-    };
   }
 })();
