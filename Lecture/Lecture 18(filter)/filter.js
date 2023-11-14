45fc258ac42c76b57fbc9d59b67f3786d5f9e1ee
urbain32 <urbainq32@gmail.com>
2023-11-14 14:46:53 +0200

    filtering an array

--- Lecture/Lecture 18(filter)/filter.js
+++ Lecture/Lecture 18(filter)/filter.js
@@ -1,2 +1,7 @@
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log("Number array: ", numberArray);
+
+// filtering an array
+var filterNumberArray = numberArray.filter(function (value) {
+  return value > 5;
+});
