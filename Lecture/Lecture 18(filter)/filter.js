e23ab890551403e280027d1ccd1190a8c0e82325
urbain32 <urbainq32@gmail.com>
2023-11-14 14:50:16 +0200

    another way to filter ana array

--- Lecture/Lecture 18(filter)/filter.js
+++ Lecture/Lecture 18(filter)/filter.js
@@ -2,7 +2,8 @@ var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log("Number array: ", numberArray);
 
 // filtering an array
-var filterNumberArray = numberArray.filter(function (value) {
+function filterFunction(value) {
   return value > 5;
-});
+}
+var filterNumberArray = numberArray.filter(filterFunction);
 console.log("filtered Number Array: ", filterNumberArray);
