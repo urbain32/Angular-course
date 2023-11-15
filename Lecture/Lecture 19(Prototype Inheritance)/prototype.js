87cc3590901e57ac170d75daa86049a69299df14
urbain32 <urbainq32@gmail.com>
2023-11-15 09:29:46 +0200

    adding prototypeJS on lecturer 19

---
+++ Lecture/Lecture 19(Prototype Inheritance)/prototype.js
@@ -0,0 +1,28 @@
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Number array: ", numberArray);
+
+// filtering an array
+function filterFunction(value) {
+  return value > 5;
+}
+var filterNumberArray = numberArray.filter(filterFunction);
+console.log("filtered Number Array: ", filterNumberArray);
+
+// filtering an array of string
+var shoppingList = [
+  "Milk",
+  "Donuts",
+  "Cookies",
+  "Chocolate",
+  "Peanut Butter",
+  "Pepto Bismol",
+  "Pepto Bismol (Chocolate flavor)",
+  "Pepto Bismol (Cookie flavor)",
+];
+var searchValue = "Bismol";
+function filterSearchedValue(value) {
+  return value.indexOf(searchValue) !== -1;
+}
+
+var filterShoppingList = shoppingList.filter(filterSearchedValue);
+console.log("filterShoppingList: ", filterShoppingList);
